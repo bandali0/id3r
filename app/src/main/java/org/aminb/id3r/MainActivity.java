f03bc43eb43c78aa93ceb947d90a3e1ced683ec8
Amin Bandali <me@aminb.org>
2014-10-10 21:30:02 -0400

    View ID3 tags proof of concept (crappy code)

--- app/src/main/java/org/aminb/id3r/MainActivity.java
+++ app/src/main/java/org/aminb/id3r/MainActivity.java
@@ -10,10 +10,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Toast;
 
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
+import com.wrapp.floatlabelededittext.FloatLabeledEditText;
+
+import java.io.IOException;
 
 
 public class MainActivity extends Activity {
+
+    protected static MainActivity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,8 @@ public class MainActivity extends Activity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        activity = this;
     }
 
 
@@ -61,6 +73,38 @@ public class MainActivity extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            Mp3File file = null;
+            try {
+                file = new Mp3File(activity.getIntent().getData().getPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (UnsupportedTagException e) {
+                e.printStackTrace();
+            } catch (InvalidDataException e) {
+                e.printStackTrace();
+            }
+            catch (NullPointerException e) {
+                // TODO: placeholder layout
+            }
+
+            if (file != null) {
+                if (file.hasId3v2Tag()) {
+                    ID3v2 tags = file.getId3v2Tag();
+                    if (tags.getTitle() != null)
+                        ((FloatLabeledEditText)rootView.findViewById(R.id.id_title)).setText(tags.getTitle());
+                    if (tags.getArtist() != null)
+                        ((FloatLabeledEditText)rootView.findViewById(R.id.id_artist)).setText(tags.getArtist());
+                    if (tags.getAlbum() != null)
+                        ((FloatLabeledEditText)rootView.findViewById(R.id.id_album)).setText(tags.getAlbum());
+
+                    // hack to make hints show up
+                    ((FloatLabeledEditText)rootView.findViewById(R.id.id_artist)).requestFieldFocus();
+                    ((FloatLabeledEditText)rootView.findViewById(R.id.id_album)).requestFieldFocus();
+                    ((FloatLabeledEditText)rootView.findViewById(R.id.id_title)).requestFieldFocus();
+                }
+            }
+
             return rootView;
         }
     }
